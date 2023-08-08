ab91d18cf03bf8ded9bd841db58552164745d7df
mary <mary@MacBook-Pro-de-mary.local>
2023-08-08 18:25:39 -0500

    Cambios

--- packages/time-range/index.ios.ts
+++ packages/time-range/index.ios.ts
@@ -1,10 +1,36 @@
-import { Screen, Color } from '@nativescript/core';
+import { Screen, Color, Application, GestureEvents } from '@nativescript/core';
 import { TimeRangeCommon } from './common';
+
+@NativeClass()
+class TimeRangerChangeHandlerImpl extends NSObject {
+  private _owner: WeakRef<TimeRange>;
+
+  public static initWithOwner(owner: WeakRef<TimeRange>): TimeRangerChangeHandlerImpl {
+    const handler = <TimeRangerChangeHandlerImpl>TimeRangerChangeHandlerImpl.new();
+    handler._owner = owner;
+    return handler;
+  }
+
+  public sliderValueChanged(sender: RangeCircularSlider) {
+    console.log('sender.endEditing(true): ', sender.endEditing(true));
+    this._owner.get().sendEvent(TimeRange.dragEvent, { start: sender.startPointValue.toString(), end: sender.endPointValue.toString() });
+    this._owner.get().sendEvent(TimeRange.dragEvent, { start: sender.startPointValue.toString(), end: sender.endPointValue.toString() });
+  }
+
+  public timerValueChanged(sender: RangeCircularSlider) {
+    console.log('timerValueChanged ', sender.startPointValue);
+    this._owner.get().sendEvent(TimeRange.timeChangeEvent, { start: sender.startPointValue.toString(), end: sender.endPointValue.toString(), event: sender.state.toString() });
+  }
+
+  public static ObjCExposedMethods = {
+    sliderValueChanged: { returns: interop.types.void, params: [RangeCircularSlider] },
+  };
+}
 
 export class TimeRange extends TimeRangeCommon {
   timeRanger: RangeCircularSlider;
   createNativeView(): Object {
-    this.timeRanger = RangeCircularSlider.alloc().init();
+    this.timeRanger = RangeCircularSlider.new().init();
     return this.timeRanger;
   }
 
@@ -17,6 +43,23 @@ export class TimeRange extends TimeRangeCommon {
     // this.timeRanger.endThumbStrokeHighlightedColor=new Color('red').ios;
     // this.timeRanger.endThumbTintColor=new Color('black').ios;
     // this.timeRanger.startThumbImage = UIImage.imageNamed("icon");
+  }
+
+  onLoaded(): void {
+    super.onLoaded();
+    const self = this.nativeView.self;
+    // refreshControl.addTargetActionForControlEvents(rHandler, "myRefresh", UIControlEvents.UIControlEventValueChanged)
+    try {
+      const _changeHandler = TimeRangerChangeHandlerImpl.initWithOwner(new WeakRef(this));
+      this.timeRanger.addTargetActionForControlEvents(_changeHandler, 'sliderValueChanged', UIControlEvents.ValueChanged);
+      this.timeRanger.addTargetActionForControlEvents(_changeHandler, 'sliderValueChanged', UIControlEvents.EditingDidEnd);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  updateData() {
+    console.log('Se ejecuto');
   }
 
   disposeNativeView(): void {
